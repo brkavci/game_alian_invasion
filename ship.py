d61c36bf9f42eba3ee74b765a071ab0d0067de1a
Burak <brkavci@hotmail.com>
2020-04-23 18:20:54 +0300

    default plane military

--- ship.py
+++ ship.py
@@ -10,7 +10,7 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('images/ships/blue_falcon.bmp')
+        self.image = pygame.image.load('images/ships/military.bmp')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
